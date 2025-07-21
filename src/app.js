1150920c58d6aecc5f15cc48788139ce58c40a74
Prabin Dangol <dprabin23@gmail.com>
2025-07-21 17:40:30 +0545

    chore:practise
    Signed-off-by: Prabin Dangol <dprabin23@gmail.com>

--- src/app.js
+++ src/app.js
@@ -2,6 +2,14 @@ import express from 'express';
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('Hello, shyam');
+});
+
+app.get('/about', (req, res) => {
+  res.send('About Page');
+});
+
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  console.log('Server is running on port 3000.....');
 });
