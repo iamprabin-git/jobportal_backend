39aaaf4c9ed3d160aaf32a4e0433fcea4c72a312
Prabin Dangol <dprabin23@gmail.com>
2025-07-23 19:51:57 +0545

    chore:practise
    Signed-off-by: Prabin Dangol <dprabin23@gmail.com>

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import fs from 'fs';
 
 const app = express();
 
@@ -10,8 +11,11 @@ app.get('/about', (req, res) => {
   res.send('About Page');
 });
 
-app.get('/contact', (req, res) => {
-  res.send('Contact Page');
+app.get('/products', (req, res) => {
+  
+  const products = fs.readFileSync('./src/data/products.json', 'utf-8');
+  const productsData = JSON.parse(products);
+  res.send(productsData);
 });
 
 app.listen(3000, () => {
