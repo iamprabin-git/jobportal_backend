ffba0ba2bc256428c21f5642b8ab0598b59dd0d2
Prabin Dangol <dprabin23@gmail.com>
2025-07-25 06:34:18 +0545

    chore:add .env
    Signed-off-by: Prabin Dangol <dprabin23@gmail.com>

--- src/app.js
+++ src/app.js
@@ -1,10 +1,20 @@
 import express from 'express';
 import fs from 'fs';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const PORT=process.env.PORT || 3000;
+const NAME=process.env.NAME;
+const VERSION=process.env.VERSION;
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, shyam');
+  res.json({
+    port: PORT, 
+    name: NAME,
+    version: VERSION,
+  });
 });
 
 app.get('/about', (req, res) => {
@@ -17,7 +27,14 @@ app.get('/products', (req, res) => {
   const productsData = JSON.parse(products);
   res.send(productsData);
 });
+app.post('/products', (req, res) => {
+  res.send('Product added successfully');
+});
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000.....');
+app.get('/not-found', (req, res) => {
+  res.status(404).send('Page not found');  
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port${PORT}.....`);
 });
