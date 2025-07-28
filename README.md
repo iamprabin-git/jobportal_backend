3fae880e33415e393c0e64273c13bc4596c53746
Prabin Dangol <dprabin23@gmail.com>
2025-07-28 19:23:29 +0545

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 #MERN stack job portal
+# ecommerce_backend
--- src/config/index.js
+++ src/config/index.js
@@ -3,8 +3,8 @@ dotenv.config();
 
 
 export default {
-    port: process.env.PORT || 3000,
-    name: process.env.APP_NAME || 'JobPortal Backend',
+    port: process.env.PORT || 5000,
+    name: process.env.APP_NAME || 'ecommerce_backend',
     version: process.env.APP_VERSION || '0.1.0',
     mongoUrl: process.env.MONGO_URL,
 };
