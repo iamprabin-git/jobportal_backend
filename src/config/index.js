6d29ebd7ebd513f5c4cd7c8f738850b10a103915
Prabin Dangol <dprabin23@gmail.com>
2025-07-28 20:01:52 +0545

    chore:correction
    Signed-off-by: Prabin Dangol <dprabin23@gmail.com>

--- src/config/index.js
+++ src/config/index.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 export default {
     port: process.env.PORT || 5000,
-    name: process.env.APP_NAME || 'ecommerce_backend',
+    name: process.env.APP_NAME || 'ecommerce-backend',
     version: process.env.APP_VERSION || '0.1.0',
     mongoUrl: process.env.MONGO_URL,
 };
